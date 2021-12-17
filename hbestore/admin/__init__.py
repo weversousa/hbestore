61502a0db56f425ccaea18a021add5b3e6cf19c5
Weverton Teixeira de Sousa <weverton.tsousa@hotmail.com>
2021-12-17 17:19:11 -0300

    all

--- hbestore/admin/__init__.py
+++ hbestore/admin/__init__.py
@@ -1,9 +1,10 @@
 from os import unlink
 from secrets import token_hex
 
-from flask import redirect, url_for
+from flask import redirect, url_for, jsonify
 from flask_admin import Admin, AdminIndexView, expose, BaseView
 from flask_admin.contrib.sqla import ModelView, form
+from flask_admin.menu import MenuLink
 from wtforms.validators import NumberRange
 from flask_login import current_user
 from werkzeug.utils import secure_filename
@@ -159,56 +160,66 @@ class ProductView(ModelView):
     }
 
 
-class RequestView(ModelView):
-    def user_name(self, request, pedido, *args):
-        user = User.query.get(pedido.user_id)
-        return user.first_name.capitalize()
+def client_name(id):
+    user = User.query.get(id)
+    return user.first_name + " " + user.last_name
 
-    def user_email(self, request, pedido, *args):
-        user = User.query.get(pedido.user_id)
-        return user.email
 
-    def produtos(self, request, pedido, *args):
-        li = []
-        for purchase in pedido.purchases:
-            p = Product.query.get(purchase.product_id)
-            li.append(f"<ID-{p.id};{p.name};{p.size.name};{p.color.name};Unidades={purchase.amount}>")
-        return li
+class RequestView(BaseView):
+    @expose("/")
+    def index(self):
+        pedidos = []    
+        for pedido in Request.query.all():
+            dict_pedido = {}
+            dict_pedido["date"] = pedido.created_on.strftime('%d/%m/%Y')
+            dict_pedido["id"] = pedido.id
+            dict_pedido["value_total"] = 0.0
+            dict_pedido["form_of_payment"] = pedido.form_of_payment
+            dict_pedido["client"] = client_name(pedido.user_id)
+            dict_pedido["address"] = via_cep2(pedido.zip_code, pedido.number)
+            dict_pedido["shipping"] = "glyphicon glyphicon-remove" if not pedido.shipping else "glyphicon glyphicon-ok"
+            dict_pedido["products"] = []
+            for obj_produto in pedido.purchases:
+                produto = Product.query.get(obj_produto.product_id)
+                dict_produto = {}
+                dict_produto["id"] = produto.id
+                dict_produto["color"] = produto.color.name
+                dict_produto["size"] = produto.size.name
+                dict_produto["brand"] = produto.brand.name
+                dict_produto["name"] = produto.name.capitalize()
+                dict_produto["price"] = formatar_moeda_real(produto.price)
+                dict_produto["amount"] = obj_produto.amount
+                dict_produto["subtotal"] = formatar_moeda_real(produto.price * obj_produto.amount)
+                dict_pedido["value_total"] += float(produto.price * obj_produto.amount)
+                dict_pedido["products"].append(dict_produto)
+            dict_pedido["value_total"] = formatar_moeda_real(f'{dict_pedido["value_total"]:.2f}')
+            pedidos.append(dict_pedido)
+        return self.render('admin-requests.html', pedidos=pedidos)
 
+
+class RequestShippingView(ModelView):
     can_edit = True
     can_delete = False
     can_create = False
 
-    column_list = ["id", "value_total", "form_of_payment", "created_on", "user", "item", "address", "shipping"]
+    column_list = ["id", "shipping"]
 
     column_labels = {
         "id": "Nº Pedido",
-        "value_total": "Valor Final R$",
-        "form_of_payment": "Forma de Pagamento",
-        "created_on": "Data da Compra",
-        "user": "Cliente",
-        "item": "Produtos",
-        "address": "Endereço de entrega",
-        "shipping": "Produto Enviado"
+        "shipping": "Enviado"
     }
 
-    column_formatters = {
-        "value_total": lambda self, request, pedido, *args: formatar_moeda_real(pedido.value_total),
-        "user": user_name,
-        "item": produtos,
-        "address": lambda self, request, pedido, *args: via_cep2(pedido.zip_code, pedido.number),
-        "created_on": lambda self, request, pedido, *args: pedido.created_on.strftime('%d/%m/%Y %H:%M'),
-    }
-
-    column_default_sort = ("created_on", True)
-
-    form_excluded_columns = ["purchases", "zip_code", "number", "id", "value_total", "form_of_payment", "created_on", "user", "item", "address"]
+    column_searchable_list = ["id"]
 
 
-class AnalyticsView(BaseView):
-    @expose('/')
-    def index(self):
-        return redirect(url_for('home.index'))
+class HomeMenuLink(MenuLink):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class LogoutMenuLink(MenuLink):
+    def is_accessible(self):
+        return current_user.is_authenticated
 
 
 admin = Admin(index_view=MyAdminIndexView())
@@ -218,13 +229,15 @@ def init_app(app):
     global upload
     upload = app.config["UPLOAD_FOLDER"]
     admin.name = "HBestore administração"
-    admin.template_mode = "bootstrap4"
-    admin.add_view(AnalyticsView(name="Site", endpoint="analytics"))
+    admin.template_mode = "bootstrap3"
     admin.add_view(UserView(User, db.session, "Usuário"))
     admin.add_view(BrandView(Brand, db.session, "Marca"))
     admin.add_view(BrandView(Category, db.session, "Categoria"))
     admin.add_view(BrandView(Color, db.session, "Cor"))
     admin.add_view(BrandView(Size, db.session, "Tamanho"))
     admin.add_view(ProductView(Product, db.session, "Produto"))
-    admin.add_view(RequestView(Request, db.session, "Pedidos"))
+    admin.add_view(RequestView(name="Pedidos", endpoint="requets-shipping"))
+    admin.add_view(RequestShippingView(Request, db.session, "Envio dos Pedidos"))
+    admin.add_link(HomeMenuLink(name="Páginia Inicial", url="/"))
+    admin.add_link(LogoutMenuLink(name="Sair", url="/auth/logout"))
     admin.init_app(app)
